46f4d6b43dfe07aea21fcaa832221ab662836b70
Gazizov Ruslan <almetfly@gmail.com>
2019-12-19 21:37:35 +0300

    Add custom orm function

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from data.models import Feedback
+
+
+def get_feedbacks() -> tuple:
+    """Get feedbacks texts and marks"""
+    feedbacks_ = [mark[0] for mark in Feedback.objects.values_list('text')]
+    marks_ = [mark[0] for mark in Feedback.objects.values_list('mark')]
+    return feedbacks_, marks_
+
+
+feedbacks, marks = get_feedbacks()
